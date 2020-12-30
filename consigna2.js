26dc8c5a5079050c1d8d868504b63698319e9783
henry sanchez <hrsd83@gmail.com>
2020-12-30 10:40:25 -0300

    arreglo de problemas

--- consigna2.js
+++ consigna2.js
@@ -1,54 +1,69 @@
 // 1- Crear un bucle que muestre en consola la tabla de multiplicar de un número definido en una variable.
 
-let numero = 5;
-for (let i = 0; i < 11; i++) {
-  console.log(numero + " * " + i + " = " + numero * i);
-}
+// let base = 9;
+// let limit = 10;
+
+// for(let i = 1; i <= limit; i++){
+//   let result = base * i;
+//   console.log(base + 'x' + i + ' = ' + result );
+// }
 
 // 2- Crear una función que determine si un número es primo.
-function numeroPrimo(primos){
-
-for (let primos = 1; primos <= 10; primos++) {
-	if (primos % 2 == 0) {
-		console.log(primos + ': Es primo');
-	}else {
-		console.log(primos + ': No es primo');
-	}
-}
-}
-numeroPrimo(10);
+// function esPrimo(numero) {
+//   // Casos especiales
+//   if (numero == 0 || numero == 1 || numero == 4) return false;
+//   for (let x = 2; x < numero / 2; x++) {
+//     if (numero % x == 0) return false;
+//   }
+//   // Si no se pudo dividir por ninguno de los de arriba, sí es primo
+//   return true;      
+// }
+// primos = 0;
+// for(let i=0; primos < 100; i++) {
+//   let cond = esPrimo(i)
+//   if(cond) {
+//     primos++;
+//     console.log(primos +' - '+ i + ' es primo');
+//   }
+// }
 
 // // 3- Mostrar los números primos entre 0 y 100.
-i = 0;
-pares = 0;
-while(pares < 50) {
-  if( i % 2 === 0) {
-    console.log(i + ' Es Primo');
-    pares += 1;
-  }
-  i += 1; 
-}
 
-// // // // 4- Mostrar los primeros 100 números primos.
+// i = 0;
+// pares = 0;
+// while( i <= 100) {
+//   if( i % 2 === 0) {
+//     console.log(i + ' Es Primo');
+//     pares += 1;
+//   }
+//   i += 1; 
+// }
 
-i = 1;
-pares = 0;
-while(pares < 50) {
-  if( i % 2 === 0) {
-    console.log(i + ' es número primo');
-    pares += 1;
-  }
-  i += 1; 
-}
+//4- Mostrar los primeros 100 números primos.
 
-// // // // 5- Crear una función que reciba dos parámetros numéricos, luego los sume, calcule el promedio y luego devuelva como resultado el promedio. Mostrar mediante alert el resultado.
+// pares = 0;
+
+// for( let i= 0; pares < 100; i++){
+//   if(i % 2 === 0 ){
+//     console.log(i + ' Es numero par')
+//     pares += 1;
+//   }
+// }
+
+// 5- Crear una función que reciba dos parámetros numéricos, luego los sume, calcule el promedio y luego devuelva como resultado el promedio. Mostrar mediante alert el resultado.
 
 
-function suma(number1, number2){
-  result = number1 +  number2
-  alert(result);
-}
-suma(150, 150 );
+// function calculateAverage(number1, number2){
+//   number1 = parseInt(prompt('Ingresar un numero:'));
+//   number2 = parseInt(prompt('Ingresa otro numero:'));
+
+//   sum = number1 + number2;
+//   average = sum / 2;
+//   return average;
+// };
+// alert('El promedio de los numeros es: ' + calculateAverage());
+
+
 
 
 
